20bceecc7a59c6a367dc9bc8d9b8bb4f9c6cd405
jeff perreton <jperreton@gmail.com>
2020-04-06 16:23:15 +0200

    simplify

--- readAvro.scala
+++ readAvro.scala
@@ -1,4 +1,4 @@
-// /home/rstudio/spark/spark-2.4.3-bin-hadoop2.7/bin/spark-shell --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0-preview,org.apache.spark:spark-avro_2.12:3.0.0-preview
+// /home/rstudio/spark/spark-3.0.0-preview-bin-hadoop3.2/bin/spark-shell --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0-preview,org.apache.spark:spark-avro_2.12:3.0.0-preview
 
 import org.apache.spark.sql.avro._
 
