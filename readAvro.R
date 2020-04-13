290f32e5dc28fc7c66251e01e5ba1210064d9e1e
jeff perreton <jperreton@gmail.com>
2020-04-13 10:09:29 +0200

    VERSIOSN

--- readAvro.R
+++ readAvro.R
@@ -45,10 +45,10 @@ value_schema_str <- '
 }
 '
 config <- spark_config()
-config$sparklyr.shell.packages <- "org.apache.spark:spark-sql-kafka-0-10_2.12:2.4.2,org.apache.spark:spark-avro_2.12:2.4.2"
+config$sparklyr.shell.packages <- "org.apache.spark:spark-sql-kafka-0-10_2.12:2.4.5"
 config$sparklyr.log.invoke <- "cat"
 
-sc <- spark_connect("spark://spark-master:7077", spark_home = "spark/spark-2.4.2-bin-hadoop2.7", config=config)
+sc <- spark_connect("spark://spark-master:7077", spark_home = "spark", config=config)
 
 # test to_avro
 
