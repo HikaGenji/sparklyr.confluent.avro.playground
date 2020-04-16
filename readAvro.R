fc62480148083bd26c64c356e158d3e5e62a0504
jeff perreton <jperreton@gmail.com>
2020-04-17 01:31:43 +0200

    u

--- readAvro.R
+++ readAvro.R
@@ -1,67 +1,13 @@
+library(sparklyudf)
 library(sparklyr)
-library(dplyr)
-library(stringr)
-library(sparkavroudf)
 
-# this cannot work as confluent avro contains more than just payload
-# solution is to implement confluent avro deserializer in a scala udf
-# https://github.com/javierluraschi/sparklyudf
-# https://stackoverflow.com/questions/48882723/integrating-spark-structured-streaming-with-the-confluent-schema-registry
-# https://blog.engineering.publicissapient.fr/2018/10/25/spark-structured-streaming-avec-kafka-schema-registry/
-# https://github.com/xebia-france/spark-structured-streaming-blog
-
-Sys.setenv(JAVA_HOME = "/usr/lib/jvm/java-8-oracle")
-
-key_schema_str <- '
-{
-   "namespace": "indicator",
-   "name": "key",
-   "type": "record",
-   "fields" : [
-     {
-       "name" : "id",
-       "type" : "string",
-       "default" : ""
-     }
-   ]
-}
-'
-
-value_schema_str <- '
-{
-   "namespace": "indicator",
-   "name": "value",
-   "type" : "record",
-    "fields" : [{
-        "name" : "timestamp",
-        "type" : {
-            "type" : "long",
-            "logicalType" : "timestamp-millis"
-        },
-        "default" : -1
-    },
-	{
-        "name" : "side",
-        "type" : "int",
-        "default" : 1
-    },
-	{
-        "name" : "id",
-        "type" : "string",
-        "default" : ""
-    }
-	]
-}
-'
 config <- spark_config()
-config$sparklyr.shell.packages <- "org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.4"
-config$sparklyr.log.invoke <- "cat"
+config$sparklyr.shell.repositories <- "http://packages.confluent.io/maven/"
 
 sc <- spark_connect("spark://spark-master:7077", spark_home = "spark", config=config)
 
-# test to_avro
+sparklyudf_register(sc)
 
-invoke_static(sc, "sparkavroudf.AvroUtils", "toAvro", spark_dataframe(sdf_len(sc, 3)), invoke_new(sc, "org.apache.spark.sql.Column", "id")) %>% sdf_register()
 
 read_options <- list(kafka.bootstrap.servers = "broker:9092",
                      subscribe = "parameter", startingOffsets="earliest")
