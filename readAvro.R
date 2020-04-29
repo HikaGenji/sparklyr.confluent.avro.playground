a1366a3b05ac668b16992eaec2829a7673a53d69
jeff perreton <jperreton@gmail.com>
2020-04-29 20:16:14 +0200

    remove defaults param

--- readAvro.R
+++ readAvro.R
@@ -5,10 +5,11 @@ library(dplyr)
 config <- spark_config()
 config$sparklyr.shell.packages <- "io.confluent:kafka-avro-serializer:5.4.1,io.confluent:kafka-schema-registry:5.4.1,org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5,org.apache.spark:spark-avro_2.11:2.4.5,za.co.absa:abris_2.11:3.1.1"
 config$sparklyr.shell.repositories <- "http://packages.confluent.io/maven/"
+# config$'sparklyr.shell.conf' <- paste0("spark.jars.ivySettings=",  "/tmp/ivysettings.xml")
 
 sc <- spark_connect(master = "local", spark_home = "spark", config=config)
 
-stream_read_kafka_avro(sc, "parameter", startingOffsets="earliest") %>%
+stream_read_kafka_avro(sc, "parameter", startingOffsets="earliest", kafkaUrl="broker:9092", schemaRegistryUrl="http://schema-registry:8081") %>%
 stream_write_memory("p")
 
 # sql style 'eager' returns an R dataframe
@@ -22,16 +23,28 @@ tbl(sc, .) %>%
 group_by(id) %>%
 summarise(n=count()) 
 
-# test write
-stream_read_kafka_avro(sc, "parameter", startingOffsets="earliest") %>%
-stream_write_kafka_avro(sc, topic="output", dataFrame=.)
+# read avro data, unstruct and push to avro data (stream_write_avro wraps columns into a struct automatically)
+stream_read_kafka_avro(sc, "parameter", startingOffsets="earliest", kafkaUrl="broker:9092", schemaRegistryUrl="http://schema-registry:8081") %>%
+invoke("select", "value.timestamp", list("value.id", "value.side")) %>%
+stream_write_kafka_avro(sc, topic="output", dataFrame=., kafkaUrl="broker:9092", schemaRegistryUrl="http://schema-registry:8081")
 
-stream_read_kafka_avro(sc, "output", startingOffsets="earliest") %>%
+# read avro data and push to memory
+stream_read_kafka_avro(sc, "output", startingOffsets="earliest", kafkaUrl="broker:9092", schemaRegistryUrl="http://schema-registry:8081") %>%
+invoke("select", "value.timestamp", list("value.id", "value.side")) %>%
+sdf_register() %>%
+mutate(side=2*side) %>%
+group_by(id) %>%
+summarise(n=count()) %>%
 stream_write_memory("q")
 
-'select value.value.timestamp as timestamp, value.value.side as side, value.value.id as id from q' %>%
+# sql style 'eager' returns an R dataframe
+query <- 'select value.timestamp, value.side, value.id from q'
+res   <- DBI::dbGetQuery(sc, statement =query)
+
+# query from memory
+'select value.timestamp as timestamp, value.side as side, value.id as id from q' %>%
 dbplyr::sql() %>%
 tbl(sc, .) %>%
-spark_dataframe()
+stream_write_memory("u")
 
 
