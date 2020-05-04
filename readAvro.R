412fc1526e29fbdaab3e5ed2f69de6e7bd09fb5c
jeff perreton <jperreton@gmail.com>
2020-05-04 23:07:47 +0200

    simplified

--- readAvro.R
+++ readAvro.R
@@ -5,15 +5,16 @@ library(dplyr)
 config <- spark_config()
 config$sparklyr.shell.packages <- "io.confluent:kafka-avro-serializer:5.4.1,io.confluent:kafka-schema-registry:5.4.1,org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5,org.apache.spark:spark-avro_2.11:2.4.5,za.co.absa:abris_2.11:3.1.1"
 config$sparklyr.shell.repositories <- "http://packages.confluent.io/maven/"
-# config$'sparklyr.shell.conf' <- paste0("spark.jars.ivySettings=",  "/tmp/ivysettings.xml")
-
+kafkaUrl <- "broker:9092"
+schemaRegistryUrl <- "http://schema-registry:8081"
 sc <- spark_connect(master = "local", spark_home = "spark", config=config)
 
-stream_read_kafka_avro(sc, "parameter", startingOffsets="earliest", kafkaUrl="broker:9092", schemaRegistryUrl="http://schema-registry:8081") %>%
-stream_write_memory("p")
+stream_read_kafka_avro(sc, "parameter", startingOffsets="earliest", kafkaUrl=kafkaUrl, schemaRegistryUrl=schemaRegistryUrl) %>%
+stream_write_kafka_avro(sc, topic="output", dataFrame=., kafkaUrl=kafkaUrl, schemaRegistryUrl=schemaRegistryUrl)
+
 
 # sql style 'eager' returns an R dataframe
-query <- 'select value.timestamp, value.side, value.id from p'
+query <- 'select * from output'
 res   <- DBI::dbGetQuery(sc, statement =query)
 
 # dbplyr style 'lazy' returns a spark dataframe stream
@@ -21,30 +22,4 @@ query %>%
 dbplyr::sql() %>%
 tbl(sc, .) %>%
 group_by(id) %>%
-summarise(n=count()) 
-
-# read avro data, unstruct and push to avro data (stream_write_avro wraps columns into a struct automatically)
-stream_read_kafka_avro(sc, "parameter", startingOffsets="earliest", kafkaUrl="broker:9092", schemaRegistryUrl="http://schema-registry:8081") %>%
-invoke("select", "value.timestamp", list("value.id", "value.side")) %>%
-stream_write_kafka_avro(sc, topic="output", dataFrame=., kafkaUrl="broker:9092", schemaRegistryUrl="http://schema-registry:8081")
-
-# read avro data and push to memory
-stream_read_kafka_avro(sc, "output", startingOffsets="earliest", kafkaUrl="broker:9092", schemaRegistryUrl="http://schema-registry:8081") %>%
-invoke("select", "value.timestamp", list("value.id", "value.side")) %>%
-sdf_register() %>%
-mutate(side=2*side) %>%
-group_by(id) %>%
-summarise(n=count()) %>%
-stream_write_memory("q")
-
-# sql style 'eager' returns an R dataframe
-query <- 'select value.timestamp, value.side, value.id from q'
-res   <- DBI::dbGetQuery(sc, statement =query)
-
-# query from memory
-'select value.timestamp as timestamp, value.side as side, value.id as id from q' %>%
-dbplyr::sql() %>%
-tbl(sc, .) %>%
-stream_write_memory("u")
-
-
+summarise(n=count())
