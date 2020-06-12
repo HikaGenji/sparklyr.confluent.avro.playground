b2c49c075c22d0b62a3e00eb01339a16194d0ff0
jeff perreton <jperreton@gmail.com>
2020-06-12 21:17:55 +0200

    opening a second spark connection to query aggregate topic

--- readAvro.R
+++ readAvro.R
@@ -6,13 +6,11 @@ config <- spark_config()
 config$sparklyr.shell.repositories <- "http://packages.confluent.io/maven/"
 kafkaUrl <- "broker:9092"
 schemaRegistryUrl <- "http://schema-registry:8081"
-sc <- spark_connect(master = "spark://spark-master:7077", spark_home = "spark", config=config)
+sc <- spark_connect(master = "spark://spark-master:7077", spark_home = "spark", config=config, app_name="parameter")
 
 stream_read_kafka_avro(sc, kafka.bootstrap.servers=kafkaUrl, schema.registry.topic="parameter", startingOffsets="latest", schema.registry.url=schemaRegistryUrl) %>%
 stream_write_kafka_avro(kafka.bootstrap.servers=kafkaUrl, schema.registry.topic="output", schema.registry.url=schemaRegistryUrl, keyCols="id", mode="update") 
 s <- stream_read_kafka_avro(sc, kafka.bootstrap.servers=kafkaUrl, schema.registry.topic="output", startingOffsets="latest", schema.registry.url=schemaRegistryUrl, name="output")
-
-# ok so this works
 
 "select side, id from parameter" %>%
 dbplyr::sql() %>%
@@ -22,17 +20,12 @@ group_by(id, time=window(timestamp, "5 minutes", "30 seconds"))%>%
 summarise(n_new=n()) %>%
 sdf_separate_column("time", into=c("start", "end")) %>%
 select(-time) %>%
-stream_write_kafka_avro(kafka.bootstrap.servers=kafkaUrl, schema.registry.topic="abcd", schema.registry.url=schemaRegistryUrl, keyCols="id", mode="update") 
+stream_write_kafka_avro(kafka.bootstrap.servers=kafkaUrl, schema.registry.topic="aggregate", schema.registry.url=schemaRegistryUrl, keyCols="id", mode="update") 
 
-# bug in stream_read_kafka: it stop the write whenever read is triggered
-
-# ok this one works
-s <- stream_read_kafka(sc, options=list(kafka.bootstrap.servers=kafkaUrl, subscribe="abcd"))
-
-# but this one does not: as soon as we try to print s it stops the query
-
-s <- stream_read_kafka_avro(sc, kafka.bootstrap.servers=kafkaUrl, schema.registry.topic="abcd",
-                                startingOffsets="latest", schema.registry.url=schemaRegistryUrl, name="test") 
+sc2 <- spark_connect(master = "spark://spark-master:7077", spark_home = "spark", config=config, app_name="aggregate")
+s <- stream_read_kafka_avro(sc2, kafka.bootstrap.servers=kafkaUrl, schema.registry.topic="aggregate",
+                                startingOffsets="latest", schema.registry.url=schemaRegistryUrl, name="aggregate") 
+							
 
 
 
